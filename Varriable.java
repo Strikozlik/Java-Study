138481f380a8ba255d964557fc498bbe2e21d9f8
Strikozlik <Saturious@i.ua>
2021-09-14 22:50:08 +0300

    First and Seccnd lessons

---
+++ Varriable.java
@@ -0,0 +1,19 @@
+public class Varriable {
+    public static void main(String[] args){
+        int myInt;
+        myInt = 557;
+        short myShort = 3245;
+        long myLong = 234234215;
+
+        double myDouble = 234.26;
+        float myFloat = 2342.6f;
+
+        char c = 'a';
+        boolean b = true;
+
+        byte myByte = 26; // -128-128
+        System.out.println(myInt);
+    }
+}
+
+

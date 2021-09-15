37ce776ae7090d5bf2dcac5c414acfef3fe39e00
Strikozlik <Saturious@i.ua>
2021-09-15 20:36:19 +0300

    Add comments noo varriables

--- Varriable.java
+++ Varriable.java
@@ -1,17 +1,17 @@
 public class Varriable {
     public static void main(String[] args){
         int myInt;
-        myInt = 557;
-        short myShort = 3245;
-        long myLong = 234234215;
+        myInt = 557;                    // цілі числа,містить 32 біт
+        short myShort = 3245;           //цілі,16 біт,від -32000 до 32000
+        long myLong = 234234215;        //цілі,64 біти,для великих чисел
 
-        double myDouble = 234.26;
-        float myFloat = 2342.6f;
+        double myDouble = 234.26;      // дробові числа,64 біт, стоїть за замовчуванням
+        float myFloat = 2342.6f;       // дробові числа,32 Біт, для присвоєння в кінці числа пишем f
 
-        char c = 'a';
-        boolean b = true;
+        char c = 'a';                  // символи,пишем в ' '
+        boolean b = true;             //
 
-        byte myByte = 26; // -128-128
+        byte myByte = 126; // -128-128
         System.out.println(myInt);
     }
 }

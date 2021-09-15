09724f61cc048f2acc432798c227b748417827d8
Strikozlik <Saturious@i.ua>
2021-09-15 21:18:12 +0300

    Lessons 3 Strings

---
+++ Strings.java
@@ -0,0 +1,11 @@
+public class Strings {
+    public static void main(String[] args){
+        int x=5;
+        String c="Hello";
+        String space=" ";
+        String name="Pavlo";
+        System.out.println(c+space+name);
+        System.out.println("Hello"+ " "+ name + " Pavlo");
+        System.out.println("My number is "+x);
+    }
+}

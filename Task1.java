355bf031575b706aacf685886c8c933428cd5a52
Strikozlik <Saturious@i.ua>
2021-09-19 12:22:53 +0300

    Task1

---
+++ Task1.java
@@ -0,0 +1,13 @@
+public class Task1 {
+    /*У змінну digits потрібно записати рядок "60".
+Використовуй змінні x, y, z, порожній рядок і конкатенацію рядків*/
+    public static void main(String [] args) {
+        int x = 2;
+        int y = 4;
+        int z = 0;
+        String digits = x + y + "" + z;
+        System.out.println(digits);
+    }
+
+
+}

d8e3d09e199d1ca138bdaf93ac0e93115c8fcad4
Strikozlik <Saturious@i.ua>
2021-10-03 11:53:52 +0300

    03.10.1

--- Tablicja_mnogen9.java
+++ Tablicja_mnogen9.java
@@ -1,12 +1,12 @@
 public class Tablicja_mnogen9 {
     public static void main(String[] args) {
 
-        for (int i = 1; i < 10; i++) {
-            for (int k = 1; k < 10; k++) {
+        for (int i = 1; i < 20; i++) {
+            for (int k = 1; k < 20; k++) {
                 if (k*i == 1)
                     System.out.print("   ");
                 else
-                System.out.print(k * i + "  ");
+                System.out.print(k * i + "    ");
             }
             System.out.println("");
         }
